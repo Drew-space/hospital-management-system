64c2af1d0c534417bc2b5e3a7124e1df64b840a5
Drew <chukwukaenudeme@gmail.com>
2026-04-21 06:42:03 +0100

    fix: handle null values in DatePicker onChange event

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -112,7 +112,7 @@ const RenderField = <T extends FieldValues>({
           />
           <DatePicker
             selected={field.value}
-            onChange={(date: Date | null) => field.onChange(date)}
+            onChange={(date: Date | null) => field.onChange(date ?? new Date())}
             dateFormat={dateFormat ?? "MM/dd/yyyy"}
             showTimeSelect={showTimeSelect ?? false}
             timeInputLabel="Time"
