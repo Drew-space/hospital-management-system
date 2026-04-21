784787f1eb3f76eef711d0880f3a6669a77353a2
Drew <chukwukaenudeme@gmail.com>
2026-04-21 21:31:26 +0100

    feat: add SMS notification functionality for appointment updates

--- lib/actions/appointment.action.ts
+++ lib/actions/appointment.action.ts
@@ -173,6 +173,7 @@ import {
   APPOINTMENT_COLLECTION_ID,
   DATABASE_ID,
   databases,
+  messaging,
 } from "../appwrite.config";
 import { formatDateTime, parseStringify } from "../utils";
 import { Appointment } from "./appwrite.types";
@@ -264,6 +265,8 @@ export const updateAppointment = async ({
     );
 
     if (!updatedAppointment) throw Error;
+    const smsMessage = `Greetings from CarePulse. ${type === "schedule" ? `Your appointment is confirmed for ${formatDateTime(appointment.schedule!, timeZone).dateTime} with Dr. ${appointment.primaryPhysician}` : `We regret to inform that your appointment for ${formatDateTime(appointment.schedule!, timeZone).dateTime} is cancelled. Reason:  ${appointment.cancellationReason}`}.`;
+    await sendSMSNotification(userId, smsMessage);
 
     revalidatePath("/admin");
     return parseStringify(updatedAppointment);
@@ -286,5 +289,20 @@ export const getAppointment = async (appointmentId: string) => {
       "An error occurred while retrieving the existing patient:",
       error,
     );
+  }
+};
+
+export const sendSMSNotification = async (userId: string, content: string) => {
+  try {
+    // https://appwrite.io/docs/references/1.5.x/server-nodejs/messaging#createSms
+    const message = await messaging.createSms(
+      ID.unique(),
+      content,
+      [],
+      [userId],
+    );
+    return parseStringify(message);
+  } catch (error) {
+    console.error("An error occurred while sending sms:", error);
   }
 };
--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -10,6 +10,7 @@ client
 export const databases = new sdk.Databases(client);
 export const storage = new sdk.Storage(client);
 export const users = new sdk.Users(client);
+export const messaging = new sdk.Messaging(client);
 
 export const {
   NEXT_PUBLIC_APPWRITE_ENDPOINT: ENDPOINT,
